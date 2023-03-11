f20b79532d9bef90ef4395c9e68b8e2388e5084e
Ulaş Alyeşil <umutulas08@gmail.com>
2023-03-11 23:13:18 +0300

    FIX: secondary button shadow

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,9 +4,9 @@ export default function Button({label, style}) {
 
     const styles = {
         primaryShort: 'flex justify-center px-6 py-3 bg-black text-white rounded-full items-center font-medium shadow-lg transition sm:hover:shadow-xl sm:hover:scale-105',
-        secondaryShort: 'flex justify-center px-6 py-3 bg-white text-neutral-400 rounded-full items-center font-medium shadow-lg transition hover:shadow-xl hover:scale-105',
+        secondaryShort: 'flex justify-center px-6 py-3 bg-white text-neutral-400 rounded-full items-center font-medium shadow-md transition hover:shadow-xl hover:scale-105',
         primaryLong: 'flex justify-center px-6 py-3 w-[320px] bg-black text-white rounded-full items-center font-medium shadow-lg transition sm:hover:shadow-2xl sm:hover:scale-105',
-        secondaryLong: 'flex justify-center px-6 py-3 w-[320px] bg-white text-neutral-400 rounded-full items-center font-medium shadow-lg hover:shadow-xl hover:font-bold'
+        secondaryLong: 'flex justify-center px-6 py-3 w-[320px] bg-white text-neutral-400 rounded-full items-center font-medium shadow-md hover:shadow-xl hover:font-bold'
     }
 
     return (
