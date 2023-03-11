06f3d383fc41b619db9031e5f237c2aa8678313e
Ulaş Alyeşil <umutulas08@gmail.com>
2023-03-11 20:32:14 +0300

    FIX: hero headline width

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Button({label, style}) {
 
     const styles = {
-        primary: 'flex px-6 py-3 bg-black text-white rounded-full items-center font-medium shadow-lg transition hover:shadow-xl hover:scale-105',
+        primary: 'flex px-6 py-3 bg-black text-white rounded-full items-center font-medium shadow-lg transition sm:hover:shadow-xl sm:hover:scale-105',
         secondary: 'flex px-6 py-3 bg-white text-black border border-neutral-900 rounded-full items-center font-medium shadow-lg hover:shadow-xl hover:font-bold'
     }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,22 @@
 import product_image from '../assets/images/product-image.png'
 import WIPInput from './WIPInput'
 
+
 export default function Hero() {
+
+  const footnote = '*Join the waitlist to get notified when FREY▲ is available on'
+
   return (
     <div className='flex flex-col items-center gap-6 sm:gap-11 mt-12'>
         <div className='flex flex-col items-center gap-2 sm:gap-4'>
             <h5 className='font-semibold text-neutral-400'>COMING SOON</h5>
-            <h1 className='font-medium text-3xl sm:text-7xl text-center'><span className='font-black'>FREY▲</span> is all your accounts in one app.</h1>
+            <h1 className='font-medium text-3xl sm:text-7xl text-center max-w-[720px]'><span className='font-black'>FREY▲</span> is all your accounts in one app.</h1>
         </div>
-        <WIPInput/>
-        <p className='text-neutral-400 text-center text-sm'>Get notified when FREY▲ is available on <span className='font-semibold'>Test Flight</span></p>
-        <img className='sm:w-96' src={product_image} alt="frey product image" />
+        <div className='flex flex-col gap-4 items-center mb-8'> 
+          <WIPInput/>
+          <p className='text-neutral-400 text-center text-sm max-w-[256px]'>{footnote}<span className='font-semibold'>Test Flight</span></p>
+        </div>
+        <img className='sm:w-96' src={product_image} alt="frey product" />
     </div>
   )
 }
