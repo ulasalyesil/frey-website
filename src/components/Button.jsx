98068189df149c659e8055db1e0451a01fc7691e
Ulaş Alyeşil <umutulas08@gmail.com>
2023-03-12 17:21:05 +0300

    FIX: button animations duration

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,8 +5,8 @@ export default function Button({label, style, onClick}) {
 
     const styles = {
         primaryShort: 'flex justify-center px-6 py-3 bg-black text-white rounded-full items-center font-medium shadow-lg transition',
-        secondaryShort: 'flex justify-center px-6 py-3 bg-white text-neutral-400 rounded-full items-center font-medium shadow-md transition hover:shadow-xl hover:scale-105',
-        primaryLong: 'flex justify-center px-6 py-3 w-[320px] bg-black text-white rounded-full items-center font-medium shadow-lg transition sm:hover:shadow-2xl sm:hover:scale-105',
+        secondaryShort: 'flex justify-center px-6 py-3 bg-white text-neutral-400 rounded-full items-center font-medium shadow-md transition ease-in duration-250 hover:shadow-xl',
+        primaryLong: 'flex justify-center px-6 py-3 w-[320px] bg-black text-white rounded-full items-center font-medium shadow-lg transition sm:hover:shadow-2xl ease-in duration-250',
         secondaryLong: 'flex justify-center px-6 py-3 w-[320px] bg-white text-neutral-400 rounded-full items-center font-medium shadow-md hover:shadow-xl hover:font-bold'
     }
 
