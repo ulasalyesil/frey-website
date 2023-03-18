59b68e078c66447793ebc38e7e26a74042e88f04
Ulaş Alyeşil <umutulas08@gmail.com>
2023-03-18 17:30:39 +0300

    ADD: footer cta, feature image change

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,8 +1,15 @@
+// packages
+import { useState } from 'react'
 import * as Accordion from '@radix-ui/react-accordion'
 import { Component1Icon as Icon} from '@radix-ui/react-icons'
-import img1 from '../assets/images/new-product-image.png'
-import Button from './Button'
 import { motion } from 'framer-motion'
+
+// components
+import Button from './Button'
+
+// assets
+import img1 from '../assets/images/feature_illustration_1.svg'
+import img3 from '../assets/images/feature_illustration_3.svg'
 
 const content = {
   "item-1": {
@@ -19,29 +26,36 @@ const content = {
   }
 }
 
+const images = [img1, img3]
+
 const itemStyle = 'flex flex-col bg-neutral-100 rounded-xl hover:bg-neutral-200 overflow-hidden first:mt-0'
-const triggerStyle = 'p-4 w-full flex gap-2 items-center'
+const triggerStyle = 'p-4 w-full flex gap-2 items-center font-medium'
 const contentStyle = 'text-sm text-neutral-500 data-[state=open]:animate-slideDown data-[state=closed]:animate-slideUp'
 
 export default function Features() {
 
-
   return (
+
     <motion.div
     className='flex flex-col gap-8 sm:gap-14 items-center'
     initial={{opacity: 0}}
     whileInView={{opacity: 1}}
     transition={{duration: 0.5}}>
-      <h2 className='font-semibold text-2xl text-neutral-500'>Features</h2>
-    <div className='flex flex-col sm:flex-row gap-8'>
 
+      <div className='flex flex-col gap-0 items-center'>
+        <h2 className='font-semibold text-2xl text-black'>Essential Perfection.</h2>
+        <h2 className='font-semibold text-2xl text-neutral-400'>Elevated.</h2>
+
+      </div>
+
+    <div className='flex flex-col sm:flex-row gap-8'>
 
       <Accordion.Root className='w-[348px] max-w-[348px] shrink-0 shadow-sm shadow-black/5 flex flex-col gap-2'
       defaultValue='item-1'
       type='single'
-      collapsible
-      >
-        <Accordion.Item value="item-1" className={itemStyle}>
+      collapsible>
+
+        <Accordion.Item id='1' value="item-1" className={itemStyle}>
             <Accordion.Trigger className={triggerStyle}>
                 <Icon/>
                 {content['item-1'].title}
@@ -49,26 +63,26 @@ export default function Features() {
               <Accordion.Content className={contentStyle}><div className='p-4 pt-0'>{content['item-1'].description}</div></Accordion.Content>
         </Accordion.Item>
 
-        <Accordion.Item value="item-2" className={itemStyle}>
-        <Accordion.Trigger className={triggerStyle}>
-                <Icon/>
-                {content['item-2'].title}
-              </Accordion.Trigger>
-              <Accordion.Content className={contentStyle}><div className='p-4 pt-0'>{content['item-2'].description}</div></Accordion.Content>
+        <Accordion.Item id='2' value="item-2" className={itemStyle}>
+          <Accordion.Trigger className={triggerStyle}>
+              <Icon/>
+              {content['item-2'].title}
+          </Accordion.Trigger>
+          <Accordion.Content className={contentStyle}><div className='p-4 pt-0'>{content['item-2'].description}</div></Accordion.Content>
         </Accordion.Item>
 
-        <Accordion.Item value="item-3" className={itemStyle}>
-        <Accordion.Trigger className={triggerStyle}>
+        <Accordion.Item id='3' value="item-3" className={itemStyle}>
+          <Accordion.Trigger className={triggerStyle}>
                 <Icon/>
                 {content['item-3'].title}
               </Accordion.Trigger>
-              <Accordion.Content className={contentStyle}><div className='p-4 pt-0'>{content['item-3'].description}</div></Accordion.Content>
+          <Accordion.Content className={contentStyle}><div className='p-4 pt-0'>{content['item-3'].description}</div></Accordion.Content>
         </Accordion.Item>
 
 
       </Accordion.Root>
 
-      <div className='w-[348px] flex justify-center shrink-1 relative bg-black rounded-3xl overflow-hidden shadow-xl'>
+      <div className='w-[348px] flex justify-center shrink-1 relative bg-neutral-100 rounded-3xl overflow-hidden shadow-xl shadow-black/10'>
         <div className='absolute bottom-4 right-4 z-50'><a href="https://tally.so/r/mOaLok" target='_blank' rel='noreferrer'><Button label={'Learn More'} style={'primarySmall'}/></a></div>
         <img src={img1} alt='feature shot' className='w-64 absolute top-10' />
       </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,36 @@
 import React from 'react'
 import logo from '../assets/images/logo-footer.svg'
+import Button from './Button'
 
 export default function Footer() {
   return (
-    <div className='flex justify-center sm:justify-between items-center w-full max-w-[1200px] mt-16 sm:mt-32 p-11'>
-      <div className='flex flex-col items-center sm:items-end sm:flex-row gap-4'>
-      <img src={logo} className='w-32' alt="frey logo" />
-      <p className='text-sm text-neutral-400'>All Rights Reserved. 2023</p>
+    <div className='mt-16 sm:mt-32 w-full flex flex-col items-center'>
+
+      <div className='flex flex-col rounded-0 sm:m-auto items-center gap-6 w-full max-w-[1200px] sm:rounded-2xl py-12 bg-neutral-100'>
+        <p className='font-medium text-neutral-500 text-xl'>Do you want to learn more?</p>
+        <a href="https://tally.so/r/mOaLok" target='_blank' rel='noreferrer'><Button label={'Join the waitlist'} style={'primaryLong'}/></a>
       </div>
-      <ol className='gap-8 hidden sm:flex'>
-        <li>
-        <a href="https://twitter.com/frey_money" target='_blank' rel='noreferrer' className='font-medium text-neutral-400'>Twitter</a>
-        </li>
-        <li>
-        <a href="mailto:ulas@frey.money" target='_blank' rel='noreferrer' className='font-medium text-neutral-400'>Contact Us</a>
-        </li>
-        <li>
-        <a href="https://frey-money.notion.site/Privacy-Policy-c1ff16017e90469cbc1530807cf117e4" target='_blank' rel='noreferrer' className='font-medium text-neutral-400'>Privacy Policy</a>
-        </li>
-        <li>
-        <a href="https://www.notion.so/frey-money/Terms-of-Use-0a642685662c480fb49fb9af675d1ef4?pvs=4" target='_blank' rel='noreferrer' className='font-medium text-neutral-400'>Terms of Use</a>
-        </li>
-      </ol>
+
+      <div className='flex justify-center sm:justify-between items-center w-full max-w-[1200px] p-11'>
+        <div className='flex flex-col items-center sm:items-end sm:flex-row gap-4'>
+        <img src={logo} className='w-32' alt="frey logo" />
+        <p className='text-sm text-neutral-400'>All Rights Reserved. 2023</p>
+        </div>
+        <ol className='gap-8 hidden sm:flex'>
+          <li>
+          <a href="https://twitter.com/frey_money" target='_blank' rel='noreferrer' className='font-medium text-neutral-400'>Twitter</a>
+          </li>
+          <li>
+          <a href="mailto:ulas@frey.money" target='_blank' rel='noreferrer' className='font-medium text-neutral-400'>Contact Us</a>
+          </li>
+          <li>
+          <a href="https://frey-money.notion.site/Privacy-Policy-c1ff16017e90469cbc1530807cf117e4" target='_blank' rel='noreferrer' className='font-medium text-neutral-400'>Privacy Policy</a>
+          </li>
+          <li>
+          <a href="https://www.notion.so/frey-money/Terms-of-Use-0a642685662c480fb49fb9af675d1ef4?pvs=4" target='_blank' rel='noreferrer' className='font-medium text-neutral-400'>Terms of Use</a>
+          </li>
+        </ol>
+      </div>
     </div>
   )
 }
